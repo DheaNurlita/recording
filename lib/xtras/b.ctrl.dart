d2dc1a233614f3c5b163c51b5e36b0135eb950dc
DheaNurlita <dheanurlitasa23@gmail.com>
2024-10-01 05:44:22 -0700

    fear: add pages

--- lib/xtras/b.ctrl.dart
+++ lib/xtras/b.ctrl.dart
@@ -5,4 +5,9 @@ abstract class Ctrl {
   static SampleSatuCtrl get sampleSatu => SampleSatuCtrl();
   static SampleDuaCtrl get sampleDua => SampleDuaCtrl();
   static SampleTigaCtrl get sampleTiga => SampleTigaCtrl();
+  static ProductListCtrl get productList => ProductListCtrl();
+
+  static ProductAddCtrl get productAdd => ProductAddCtrl();
+  static ProductDetailCtrl get productDetail => ProductDetailCtrl();
+  static ProductEditCtrl get productEdit => ProductEditCtrl();
 }
